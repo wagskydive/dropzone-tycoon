b9a3dd900128154456942483ed601300209668ef
Willem de Groot <wagskydive@gmail.com>
2020-11-27 17:54:11 +0100

    Added testing assembly and first testing scripts

---
+++ Assets/Tests/CharacterTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using CharacterLogic;
+
+namespace Tests
+{
+    public class CharacterTests
+    {
+        [Test]
+        public void can_create_a_random_character_and_verify_exists()
+        {
+            CharacterDataHolder characterDataHolder = new CharacterDataHolder();
+
+            Character character = CharacterDataCreator.CreateRandomCharacter(characterDataHolder, 1, 2);
+
+            Assert.NotNull(CharacterDataSupplier.AllCharacterNames(characterDataHolder));
+            Assert.AreEqual(character, CharacterDataSupplier.AllCharacterNames(characterDataHolder)[CharacterDataSupplier.AllCharacterNames(characterDataHolder).Length-1]);
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ UnitTesting/UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f22b5d5-2bf0-424a-b986-703201e3d7ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
